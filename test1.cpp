972c79254b4bd871cb1a1f7813dc37c3a2c71729
patrickGoo <PatrickGoo@github.com>
2018-04-16 16:22:48 +0900

    xx add

---
+++ test1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  for(int a; a<10; a++;){
+    for(int b; b<10; b++;){
+      cout << a << " X " << b << " = " << a*b << endl;
+    }
+  }
+
+  return 0;
+
+}
